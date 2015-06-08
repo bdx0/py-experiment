7b934b5dc36ebde360287a807c79d81564ad12d1
Duong Bao Duy <baoduy.duong0206@gmail.com>
2015-06-08 14:15:33 +0700

    - simple client
    Signed-off-by: Duong Bao Duy <baoduy.duong0206@gmail.com>

--- src/py-learn/src/org/dbd/py/learn/network/client.py
+++ src/py-learn/src/org/dbd/py/learn/network/client.py
@@ -4,10 +4,17 @@ follow: http://www.tutorialspoint.com/python/python_networking.htm
 '''
 import socket  # Import socket module
 
-s = socket.socket()  # Create a socket object
+#create an INET, STREAMing socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = 'google.com'  # Get local machine name
 port = 80  # Reserve a port for your service.
 
 s.connect((host, port))
-print s.recv(1024)
+s.send('GET / HTTP/1.1 \n\n\n')
+data = None
+while 1:
+    data = s.recv(1024)
+    if not data:
+        break
+    print data 
 s.close  # Close the socket when done
